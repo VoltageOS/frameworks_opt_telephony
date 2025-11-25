9ab4f0c096d2de7c71f060dbc496b008913a18ec
xunwang <xunwang@asrmicro.com>
2025-11-25 17:59:08 +0800

    Limit SIM contact fetch retries and handle capacity errors
    When fetching the SIM phonebook capacity
    fails (EVENT_GET_PHONEBOOK_CAPACITY_DONE returns an exception), pending
    read/write operations were not notified of the error. This could cause
    subsequent retries for fetching contacts to loop indefinitely, leading
    to hangs and a poor user experience.
    
    Bug: 463794708
    Test: atest SimPhonebookRecordCacheTest
    Change-Id: Ic00077091d1dc1ff02a856c937c5d1f15fd3c016
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/java/com/android/internal/telephony/uicc/SimPhonebookRecordCache.java
+++ src/java/com/android/internal/telephony/uicc/SimPhonebookRecordCache.java
@@ -362,6 +362,7 @@ public class SimPhonebookRecordCache extends Handler {
                         mAdnCapacity.set(new AdnCapacity());
                     }
                     invalidateSimPbCache();
+                    responseToWaitersWithErrorOrSuccess(false);
                 }
                 break;
             case EVENT_PHONEBOOK_RECORDS_RECEIVED:
@@ -483,6 +484,9 @@ public class SimPhonebookRecordCache extends Handler {
                 populateAdnRecords(records.getPhonebookRecords());
             } else if (records.isCompleted()) {
                 logd("The whole loading process is finished");
+                if (hasMessages(EVENT_GET_PHONEBOOK_RECORDS_RETRY)) {
+                    removeMessages(EVENT_GET_PHONEBOOK_RECORDS_RETRY);
+                }
                 populateAdnRecords(records.getPhonebookRecords());
                 mIsRecordLoading.set(false);
                 mIsInRetry.set(false);
@@ -575,7 +579,7 @@ public class SimPhonebookRecordCache extends Handler {
             removeMessages(EVENT_GET_PHONEBOOK_RECORDS_RETRY);
         }
         mIsInRetry.set(true);
-        Message message = obtainMessage(EVENT_GET_PHONEBOOK_RECORDS_RETRY, 1, 0);
+        Message message = obtainMessage(EVENT_GET_PHONEBOOK_RECORDS_RETRY, times, 0);
         sendMessageDelayed(message, RETRY_INTERVAL);
     }
 
